502f67eb8093119918d6b44ca847fcf474649f90
Adham Hossam <adham.marey4@yahoo.com>
2021-12-09 17:41:28 +0200

    Add User Unit Tests

---
+++ GuestBook.UnitTests/GuestBookUserTests.cs
@@ -0,0 +1,48 @@
+﻿using GuestBook.Controllers;
+using GuestBook.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuestBook.UnitTests
+{
+ 
+    [TestClass]
+    public class GuestBookUserTests
+    {
+
+        [TestMethod]
+        public void GetFullName_CapitalLetter_returnsTrue()
+        {
+            GuestBookUser user = new GuestBookUser(1, "adham", "1234", "adham", "hossam");
+            string captialname = "Adham Hossam";
+            Assert.IsTrue(captialname == user.getFullName());
+        }
+
+        [TestMethod]
+        public void GetFullName_SmallLetter_returnsFalse()
+        {
+            GuestBookUser user = new GuestBookUser(1, "adham", "1234", "adham", "hossam");
+            string captialname = "Adham hossam";
+            Assert.IsFalse(captialname == user.getFullName());
+        }
+        [TestMethod]
+        public void CapitalizeName_CapitalLetter_returnsTrue()
+        {
+            GuestBookUser user = new GuestBookUser(1, "adham", "1234", "adham", "hossam");
+
+           
+            string captialname = "Sayed";
+            Assert.IsTrue(captialname == user.CapitalizeFirstLetter("sayed"));
+        }
+        public void CapitalizeName_SmallLetter_returnsFalse()
+        {
+            GuestBookUser user = new GuestBookUser(1, "adham", "1234", "adham", "hossam");
+
+
+            string captialname = "sayed";
+            Assert.IsFalse(captialname == user.CapitalizeFirstLetter("sayed"));
+        }
+    }
+}
